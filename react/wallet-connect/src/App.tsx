3bfc9a2a2e632bf231f879022eaa27db629742d3
LeondavidZipp <leondavidzipp@gmx.de>
2024-07-29 09:29:26 +0000

    error adjustments in main && functions

--- react/wallet-connect/src/App.tsx
+++ react/wallet-connect/src/App.tsx
@@ -1,55 +1,80 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { ConnectPhantom } from './functions'
-import { PhantomProvider } from './interfaces'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { ConnectPhantom, DisconnectPhantom, GetProvider } from "./functions";
+import { PhantomProvider } from "./interfaces";
 
 function App() {
-    const [count, setCount] = useState(0)
-    const [wallet, setProvider] = useState<PhantomProvider | undefined>();
+    const [provider, setProvider] = useState<PhantomProvider | undefined>(
+        undefined
+    );
+    const [walletKey, setWalletKey] = useState<string>("");
 
     useEffect(() => {
-        const provider = await ConnectPhantom();
+        const provider = GetProvider();
 
-        if (provider) setProvider(provider);
-        else setProvider(undefined);
+        if (provider) {
+            setProvider(provider);
+        } else {
+            setProvider(undefined);
+        }
     }, []);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Please connect your Wallet</h1>
-          <div className="card">
-              <div className="buttonContainer">
-                  {wallet == "" && (<button onClick={() => setProvider((wallet) => {
-        })}>
-          Connect Wallet
-        </button>)}
-                  {wallet != "" &&
-                      (<><button onClick={() => setCount((count) => count + 1)}>
-          Disconnect Wallet
-              </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-        Sign Example Transaction
-                </button></>)}
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Please connect your Wallet</h1>
+            <div className="card">
+                <div className="buttonContainer">
+                    {walletKey == "" && (
+                        <button
+                            onClick={async () => {
+                                const publicKey = await ConnectPhantom();
+                                setWalletKey(publicKey);
+                            }}
+                        >
+                            Connect Wallet
+                        </button>
+                    )}
+                    {walletKey != "" && (
+                        <>
+                            <button
+                                onClick={async () => {
+                                    const success = await DisconnectPhantom(
+                                        walletKey
+                                    );
+                                    if (success) setWalletKey("");
+                                }}
+                            >
+                                Disconnect Wallet
+                            </button>
+                            <button onClick={() => {}}>
+                                Sign Example Transaction
+                            </button>
+                        </>
+                    )}
                 </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
--- react/wallet-connect/src/functions.tsx
+++ react/wallet-connect/src/functions.tsx
@@ -1,30 +1,57 @@
 import {
-    Connection, SystemProgram, Transaction,
-    LAMPORTS_PER_SOL, clusterApiUrl
-} from '@solana/web3.js';
-import {
-    getExplorerLink
-} from '@solana-developers/helpers';
-import { PhantomProvider } from './interfaces';
+    Connection,
+    SystemProgram,
+    Transaction,
+    LAMPORTS_PER_SOL,
+    clusterApiUrl,
+} from "@solana/web3.js";
+import { getExplorerLink } from "@solana-developers/helpers";
+import { PhantomProvider } from "./interfaces";
 
-let connection = new Connection(clusterApiUrl('testnet'), 'confirmed');
+const connection = new Connection(clusterApiUrl("testnet"), "confirmed");
 
-export async function ConnectPhantom(): Promise<PhantomProvider | undefined> {
-    const solana = (window as any).solana;
+declare global {
+    interface Window {
+        solana: any;
+    }
+}
+
+export function GetProvider(): PhantomProvider | undefined {
+    if ("solana" in window) {
+        const { provider } = window.solana;
+        if (provider.isPhantom) {
+            return provider as PhantomProvider;
+        }
+    }
+    return undefined;
+}
+
+export async function ConnectPhantom(): Promise<string> {
+    const { solana } = window;
     if (solana) {
         try {
             const response = await solana.connect();
             console.log(response.publicKey.toString());
-            return response;
+            return response.publicKey.toString();
         } catch (e) {
             console.error(e);
+            return ("");
         }
     } else {
-        console.error('Phantom wallet not found');
-        return undefined;
+        console.error("Phantom wallet not found");
+        return "";
     }
 }
 
+export async function DisconnectPhantom(walletKey: string): Promise<boolean> {
+    const { solana } = window;
+
+    if (walletKey && solana) {
+        await (solana as PhantomProvider).disconnect();
+        return true;
+    }
+    return false;
+}
 // Function to sign a transaction
 async function SignTx() {
     const provider = await ConnectPhantom();
@@ -35,15 +62,17 @@ async function SignTx() {
             SystemProgram.transfer({
                 fromPubkey: provider.publicKey,
                 toPubkey: provider.publicKey,
-                lamports: 0.5 * LAMPORTS_PER_SOL
+                lamports: 0.5 * LAMPORTS_PER_SOL,
             })
         );
         console.log(`Connected to cluster2:`);
         const signedTx = await provider.signTransaction(tx);
         const serializedTx = signedTx.serialize();
         const signature = await connection.sendRawTransaction(serializedTx);
-        console.log(`Transaction sent: ${getExplorerLink("tx", signature, "testnet")}`);
+        console.log(
+            `Transaction sent: ${getExplorerLink("tx", signature, "testnet")}`
+        );
     } else {
-        console.error('Phantom wallet not found');
+        console.error("Phantom wallet not found");
     }
 }
